e041b252f86644f30f6513d67af38444f3538c06
tyfta9 <lazar2den@gmail.com>
2024-09-23 16:31:54 +0100

    commit #5 - finished commests, vertion 1.0

--- EuclideanAlg.c
+++ EuclideanAlg.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // Function prototypes
 unsigned int GetGCD(unsigned int num1, unsigned int num2);
@@ -8,30 +9,30 @@ int main(int argc, char *argv[])
 {
   printf("\nThis program finds GCD & LCM\n\n");
 
-  //
-  char key;
-
   // Getting input from the user
-  unsigned int num1 = GetNumber("Write first integer: ");
-  unsigned int num2 = GetNumber("Write second integer: ");
+  unsigned long num1 = GetNumber("Write first integer: ");
+  unsigned long num2 = GetNumber("Write second integer: ");
 
-  // Calculating Greatest Common Deviser
-  // Smallest number goes first
+  // Calculating Greatest Common Divisor (GCD)
+  // Smaller number goes first
   unsigned int gcd = 0;
   if (num1 < num2)
     gcd = GetGCD(num1, num2);
   else
     gcd = GetGCD(num2, num1);
 
-  // Calculating Lowest Common Multiple
-  int lcm = num1 * num2 / gcd;
+  // Calculating Lowest Common Multiple (LCM)
+  unsigned long lcm = num1 * num2 / gcd;
 
-  printf("\nYour GCD is: %d\n", gcd);
-  printf("Your LCM is: %d\n\n", lcm);
+  // Correct output of GCD and LCM
+  printf("\nYour GCD is: %u\n", gcd);  // Use %u for unsigned int
+  printf("Your LCM is: %lu\n\n", lcm); // Use %lu for unsigned long
 
-  printf("Press any key..");
-  scanf("%c", &key);
-  scanf("%c", &key);
+  // End of the program, wait for key
+  printf("Press any key to exit...");
+
+  getchar(); // Capture leftover newline
+  getchar(); // Wait for key press
 
   return 0;
 }
@@ -40,15 +41,17 @@ unsigned int GetNumber(char *prom)
 {
   unsigned int num = 0;
 
-  printf("%d", prom);
-  scanf("%d", &num);
+  // Prompt user for input
+  printf("%s", prom); // Use %s to print string
+  scanf("%u", &num);  // Use %u for unsigned int
 
   return num;
 }
 
-int GetGCD(int num1, int num2)
+unsigned int GetGCD(unsigned int num1, unsigned int num2)
 {
-  int remainder = num1 % num2;
+  // Calculating Greatest Common Divisor using recursion
+  unsigned int remainder = num1 % num2;
   if (remainder == 0)
     return num2;
   return GetGCD(num2, remainder);
